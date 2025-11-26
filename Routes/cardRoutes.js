c94ed1fa658ff2055a660b2753cfc13d977a945f
bruno.falcao <brunocesar.sf.16@gmail.com>
2025-11-26 04:48:30 -0400

    Routes were added

---
+++ Routes/cardRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const cardController = require("../Controllers/cardController");
+
+router.post("/cards/", cardController.createCard);
+router.get("/cards/", cardController.getCards);
+router.get("/cards/:id", cardController.getCardById);
+router.put("/cards/:id", cardController.updateCard);
+router.delete("/cards/:id", cardController.deleteCard);
+
+module.exports = router;
